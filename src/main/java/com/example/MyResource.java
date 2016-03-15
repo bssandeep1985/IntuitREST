1edbc9ca694597ea71c776d38fc2b7d8ef9a4c48
SimonHGR <simon@dancingcloudservices.com>
2016-03-15 12:38:32 -0600

    Added Response examples, and slide deck

--- src/main/java/com/example/MyResource.java
+++ src/main/java/com/example/MyResource.java
@@ -10,69 +10,107 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.ResponseBuilder;
 import javax.ws.rs.core.UriInfo;
 
 /**
  * Root resource (exposed at "myresource" path)
  */
 @Path("/myresource")
-//@Singleton
+// @Singleton
 public class MyResource {
-	@HeaderParam("x-simon") String simonHeader;
-	@HeaderParam("x-fred") String fredHeader;
+	@HeaderParam("x-simon")
+	String simonHeader;
+	@HeaderParam("x-fred")
+	String fredHeader;
 	{
 		System.out.println("Instantiating MyResource...");
 	}
-    /**
-     * Method handling HTTP GET requests. The returned object will be sent
-     * to the client as "text/plain" media type.
-     *
-     * @return String that will be returned as a text/plain response.
-     */
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String getIt() {
-        return "Got it! simon header is " + simonHeader
-        		+ " fredHeader is " + fredHeader;
-    }
-    
-    @GET
-    @Path("/otherstuff")
-    public String getOtherStuff(@QueryParam("name") String name, 
-    		@QueryParam("address") String address
-//    		, @DefaultValue("UNSET") @HeaderParam("x-simon-header") String simonHeader
-    		) {
-    	return "other stuff, name is " + name
-    			+ " and address is " + address 
-    			+ " simon header is " + simonHeader
-        		+ " fredHeader is " + fredHeader;
-    }
-    
-    @DELETE	
-    @Path("/otherstuff")
-    public String deleteOtherStuff() {
-    	return "DELETED other stuff";
-    }
-    
-    // resource e.g. .../customers/<pk>
-    @GET
-    @Path("/customers/ID-{id: \\d+}")
-    public String getOneCustomer(@PathParam("id") String theId) {
-    	return "This is customer " + theId;
-    }
-    @GET
-    @Path("/headers")
-    public String getAllHeaders(@Context HttpHeaders headers) {
-    		String name = headers.getHeaderString("x-name");
-    		return "name is " + name;
-    }
-    
-    @GET
-    @Path("/path/{path: [a-zA-Z0-9/-]*}")
-    public String getFancyPath(@PathParam("path") String path,
-    		@Context UriInfo uriInfo) {
-    	
-    	return "path is: " + path
-    			+ " absolute is: " + uriInfo.getAbsolutePath();
-    }
+
+	/**
+	 * Method handling HTTP GET requests. The returned object will be sent to
+	 * the client as "text/plain" media type.
+	 *
+	 * @return String that will be returned as a text/plain response.
+	 */
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String getIt() {
+		return "Got it! simon header is " + simonHeader + " fredHeader is " + fredHeader;
+	}
+
+	@GET
+	@Path("/otherstuff")
+	public String getOtherStuff(@QueryParam("name") String name, @QueryParam("address") String address
+	// , @DefaultValue("UNSET") @HeaderParam("x-simon-header") String
+	// simonHeader
+	) {
+		return "other stuff, name is " + name + " and address is " + address + " simon header is " + simonHeader
+				+ " fredHeader is " + fredHeader;
+	}
+
+	@DELETE
+	@Path("/otherstuff")
+	public String deleteOtherStuff() {
+		return "DELETED other stuff";
+	}
+
+	// resource e.g. .../customers/<pk>
+	@GET
+	@Path("/customers/ID-{id: \\d+}")
+	public String getOneCustomer(@PathParam("id") String theId) {
+		return "This is customer " + theId;
+	}
+
+	@GET
+	@Path("/headers")
+	public String getAllHeaders(@Context HttpHeaders headers) {
+		String name = headers.getHeaderString("x-name");
+		return "name is " + name;
+	}
+
+	@GET
+	@Path("/path/{path: [a-zA-Z0-9/-]*}")
+	public String getFancyPath(@PathParam("path") String path, @Context UriInfo uriInfo) {
+
+		return "path is: " + path + " absolute is: " + uriInfo.getAbsolutePath();
+	}
+
+	@Path("/response")
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public Response getResponse() {
+		ResponseBuilder rb = Response.ok();
+
+		if (Math.random() > 0.5) {
+			rb.entity("Simple response, it worked...");
+			rb.header("x-something", "something interesting as a value");
+		} else {
+			rb.entity("Broken! ouch");
+			rb.status(Response.Status.NOT_FOUND);
+			rb.header("x-reason", "you asked a silly question");
+		}
+
+		return rb.build();
+	}
+
+	@Path("/response")
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public Response getHttpResponse() {
+		ResponseBuilder rb = Response.ok();
+
+		if (Math.random() > 0.5) {
+			rb.entity("<HTML><BODY><p>HTML response, it worked...</P></BODY></HTML>");
+			rb.header("x-something", "something interesting as a value");
+		} else {
+			rb.entity("Broken HTML! ouch");
+			rb.type(MediaType.TEXT_PLAIN);
+			rb.status(Response.Status.NOT_FOUND);
+			rb.header("x-reason", "you asked a silly question");
+		}
+
+		return rb.build();
+	}
 }
